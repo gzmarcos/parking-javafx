6bee62f00361ec4f98cfab0d92fd431b39f07c27
Marcos G. Zimmermann <mgzmaster@gmail.com>
2018-09-20 00:02:45 -0300

    Add sample GridPane to make sure that javaFX is working

--- src/main/java/br/org/catolicasc/parking/Main.java
+++ src/main/java/br/org/catolicasc/parking/Main.java
@@ -1,14 +1,23 @@
 package br.org.catolicasc.parking;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
 
 public class Main extends Application {
 
-  public void start(Stage primaryStage) throws Exception {
-
+  @Override
+  public void start(Stage primaryStage) throws Exception{
+    Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+    primaryStage.setTitle("Example");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
   }
-  public static void main(String[] args) {
 
+
+  public static void main(String[] args) {
+    launch(args);
   }
 }
---
+++ src/main/java/br/org/catolicasc/parking/SampleController.java
@@ -0,0 +1,5 @@
+package br.org.catolicasc.parking;
+
+public class SampleController {
+
+}
