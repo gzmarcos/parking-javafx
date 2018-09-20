bdd20be7253d28ee56e7f8c60afe94cc7040dbe8
Marcos G. Zimmermann <mgzmaster@gmail.com>
2018-09-19 21:48:40 -0300

    Create mave project and include javafx dependency :rocket:

---
+++ src/main/java/br/org/catolicasc/parking/Main.java
@@ -0,0 +1,14 @@
+package br.org.catolicasc.parking;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  public void start(Stage primaryStage) throws Exception {
+
+  }
+  public static void main(String[] args) {
+
+  }
+}
