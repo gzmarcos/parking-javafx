03d0919d18aa823e711dd30a8c7dbe2fd93af903
Marcos G. Zimmermann <mgzmaster@gmail.com>
2018-09-20 00:07:11 -0300

    Add a close button to the sample window

--- src/main/java/br/org/catolicasc/parking/SampleController.java
+++ src/main/java/br/org/catolicasc/parking/SampleController.java
@@ -1,5 +1,14 @@
 package br.org.catolicasc.parking;
 
-public class SampleController {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+public class SampleController {
+  public Button closeButton;
+
+  public void handleCloseButtonAction(ActionEvent event) {
+    Stage stage = (Stage) closeButton.getScene().getWindow();
+    stage.close();
+  }
 }
