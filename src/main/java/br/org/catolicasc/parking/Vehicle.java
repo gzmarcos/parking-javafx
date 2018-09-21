92e576c859f6f0d3f4823aacad4f256ff48f7840
EsdrasXavier <esdrasxa@gmail.com>
2018-09-21 19:44:30 -0300

    Create Veichle class and add myself to developers into xml

---
+++ src/main/java/br/org/catolicasc/parking/Vehicle.java
@@ -0,0 +1,19 @@
+package br.org.catolicasc.parking;
+
+public class Vehicle {
+    private String color;
+    private String model;
+    private String plate;
+
+    public String getColor() { return this.color; }
+
+    public void setColor(String color) { this.color = color; }
+
+    public String getModel() { return model; }
+
+    public void setModel(String model) { this.model = model; }
+
+    public String getPlate() { return plate; }
+
+    public void setPlate(String plate) { this.plate = plate; }
+}
